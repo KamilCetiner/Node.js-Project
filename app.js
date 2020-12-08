0f8fd97d7811165ce98365e2cd763265ca6884b7
KamilCetiner <cetiner.kamil84@gmail.com>
2020-12-08 20:28:49 +0100

    add userpage

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,17 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-// RESTFULL API
-// USERS API
-
-// CREATE - add user
-// CRUD - 
-// READ- list user
-// UPDATE -
-// DELETE
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,7 +33,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
